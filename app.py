c0f238509cc6f3b5e2c3a81a9f1f0ef4f263c62f
Pushpakant Shindeclc <pushpakantshinde@gmail.com>
2023-08-19 14:09:31 +0530

    developer a work

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def multiplication(a,b):
     result = a * b
     return result
 
+def square_input(a,b):
+    result1 = a * a
+    result2 = b * b
+
 print(addition(5, 10))
 print(multiplication(5, 10))
