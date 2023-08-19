bce5328ce32f2f3934a50eac5ad083454af09bfb
Pushpakant Shindeclc <pushpakantshinde@gmail.com>
2023-08-19 14:11:07 +0530

    developer b work

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def multiplication(a,b):
     result = a * b
     return result
 
+def square_root(a,b):
+    result1 = a ** 0.5
+    result2 = b ** 0.5
+
+
 print(addition(5, 10))
 print(multiplication(5, 10))
